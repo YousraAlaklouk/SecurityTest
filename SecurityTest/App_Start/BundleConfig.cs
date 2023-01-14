8102f4108292e1f4f8a731579f0adf6f048e0f27
Yousra Alaklouk <100789873+YousraAlaklouk@users.noreply.github.com>
2023-01-14 15:23:59 +0300

    bootstrap

--- SecurityTest/App_Start/BundleConfig.cs
+++ SecurityTest/App_Start/BundleConfig.cs
@@ -8,23 +8,22 @@ namespace SecurityTest
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            StyleBundle(bundles);
+            ScriptBundle(bundles);
+        }
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+        public static void StyleBundle(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css")
+                     .Include("~/Content/bootstrap-special.css"));
+        }
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+        public static void ScriptBundle(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/js")
+                     .Include("~/Scripts/jquery-{version}.js")
+                     .Include("~/Scripts/bootstrap.js"));
         }
     }
+
 }
